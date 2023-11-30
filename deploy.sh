35915198d648bd02bd1a745e826d7196ea42eaeb
gnobaaaar <eumdengs@gmail.com>
2023-11-30 11:15:25 +0900

    second for devops

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,27 @@
-REPOSITORY=/home/dwadmin/cicd
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/git-action-test
 cd $REPOSITORY
 
-APP_NAME=curriculum #1
+APP_NAME=demo
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ] #2
-then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+# =====================================
+# 현재 구동 중인 application pid 확인
+# =====================================
+CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
+
+if [ -z "$CURRENT_PID" ]; then
+    echo "NOT RUNNING"
 else
-  echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -9 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> $JAR_PATH 배포" #3
-nohup java -jar /home/dwadmin/cicd/*SNAPSHOT.jar  > /home/dwadmin/cicd/nohup.log 2>&1 &
+echo "> $JAR_PATH 배포"
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+#nohup java -jar /home/dwadmin/cicd/*SNAPSHOT.jar  > /home/dwadmin/cicd/nohup.log 2>&1 &
 #nohup java -jar /home/ec2-user/app/build/libs/curriculum-1.0.jar --spring.config.location=/home/ec2-user/application.yml > /dev/null 2> /dev/null < /dev/null &
